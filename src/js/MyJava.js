985be5f93d0139bd3e02ab0fb18ea55122ed7c9f
fatalb10w <98054656+fatalb10w@users.noreply.github.com>
2022-11-17 19:08:27 +0400

    Добавлен мета тег
    Добавлен метатег ограничивающий прокрутку на мобильных устройствах

---
+++ src/js/MyJava.js
@@ -0,0 +1,30 @@
+let site, design, adaptive;
+
+let calc = {
+    price: [
+        [100, 500, 1000],
+        [500 1000 2000],
+        [1000 2000 5000],
+    ],
+    
+    days[
+    [1, 2, 3],
+    [3, 2, 4],
+    [1, 5 ,6],
+    run(sitem design, adaptive){
+    
+}
+}
+
+function() getAnswers{
+    site=promt("Какой тип сайта? 1- Сайт-визитка 2-Интернет магазин");
+    if(sute !=1 && site != 2 && site != 3){
+        alert("Такого варианта нет");
+        getAnswers();
+        return;
+    }
+    design=promt("Какой тип дизайна? 1- Шаблон 2-Уникальный");
+    
+    adaptive=promt("Какой тип адаптивности? 1- Только ПК 2- ПК + Мобильная");
+    
+}
